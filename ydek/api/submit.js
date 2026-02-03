f6510eafbe75d50c21163bec16d5915deac40350
Naaji <aliamjadian.3330@gmail.com>
2026-02-03 23:04:07 +0330

    Add files via upload

---
+++ ydek/api/submit.js
@@ -0,0 +1,15 @@
+﻿export default async function handler(req, res) {
+  if (req.method !== "POST")
+    return res.status(405).send("Method Not Allowed");
+
+  const GOOGLE_SCRIPT_URL =
+    "اینجا_لینک_وب_اپ_گوگل_شیت_تو";
+
+  await fetch(GOOGLE_SCRIPT_URL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(req.body)
+  });
+
+  res.json({ status: "ok" });
+}
